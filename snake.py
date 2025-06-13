f21fd9160debe0637bdb1b274eeff461597516b0
Maximo Zweers <maximozweers@gmail.com>
2025-06-13 15:25:44 +0200

    Input detection is now continuous

--- snake.py
+++ snake.py
@@ -4,11 +4,11 @@ import keyboard # type: ignore
 import os
 
 # Ask the user for the map size
-x_map_size = int(input("x: "))
-y_map_size = int(input("y: "))
+x_map_size = 20 #int(input("x: "))
+y_map_size = 20 #int(input("y: "))
 
 # Give the user a bit of time before the game begins
-time.sleep(1)
+time.sleep(.1)
 
 # Initialize color codes
 reset_color = "\033[0m"
@@ -27,7 +27,10 @@ segments = [
 direction = [0, -1] # The player starts facing up
 
 ITERATION_DURATION = 0.15 # The time in between movement steps
+time_to_pass = 0
+
 grow_buffer = 0
+
 
 # A function that keeps generating a random position within the playing field, and returns it when it's unoccupied
 def spawn_food():
@@ -81,51 +84,53 @@ def Rendering():
     print("Score: " + str(score))
 
 while True:
-    os.system("cls" or "clear") # Clear the terminal screen before drawing 
+
     Input()
-    new_head = [segments[0][0] + direction[0], segments[0][1] + direction[1]] # The position of the new head will be the old head + the direction
+    if (time.time() > time_to_pass):
+        time_to_pass = time.time() + ITERATION_DURATION
+        os.system("cls" or "clear") # Clear the terminal screen before drawing 
+        new_head = [segments[0][0] + direction[0], segments[0][1] + direction[1]] # The position of the new head will be the old head + the direction
 
-    if new_head in segments or new_head[0] < 0 or new_head[0] >= x_map_size or new_head[1] < 0 or new_head[1] >= y_map_size:
-        game_over = True # If the position of the new head overlaps the borders or the snake itself, game over
+        if new_head in segments or new_head[0] < 0 or new_head[0] >= x_map_size or new_head[1] < 0 or new_head[1] >= y_map_size:
+            game_over = True # If the position of the new head overlaps the borders or the snake itself, game over
 
-    segments.insert(0, new_head) # Add the seperate new head position to the list of all snake segments (specifically at the beginning of the list)
+        segments.insert(0, new_head) # Add the seperate new head position to the list of all snake segments (specifically at the beginning of the list)
 
-    if new_head == apple:
-        ITERATION_DURATION *= 0.99 # Increase the speed of which the game plays out
-        score += 1
-        grow_buffer += 1 # The player will grow with one
+        if new_head == apple:
+            ITERATION_DURATION *= 0.99 # Increase the speed of which the game plays out
+            score += 1
+            grow_buffer += 1 # The player will grow with one
 
-        # One in five chance for a golden apple to spawn
-        if random.randint(1, 5) == 1: 
-            gold_apple = spawn_food()
-            apple = [-1, -1] # If a golden apple is spawned, hide the normal apple outside the map
-        else:
+            # One in five chance for a golden apple to spawn
+            if random.randint(1, 5) == 1: 
+                gold_apple = spawn_food()
+                apple = [-1, -1] # If a golden apple is spawned, hide the normal apple outside the map
+            else:
+                apple = spawn_food()
+                gold_apple = [-1, -1] # If a normal apple is spawned, hide the golden apple
+
+        elif new_head == gold_apple:
+            ITERATION_DURATION *= 0.99
+            score += 5
+            grow_buffer += 5 
             apple = spawn_food()
-            gold_apple = [-1, -1] # If a normal apple is spawned, hide the golden apple
+            gold_apple = [-1, -1] # Hide the golden apple outside the map regardless, so there cannot be two golden apples in a row
 
-    elif new_head == gold_apple:
-        ITERATION_DURATION *= 0.99
-        score += 5
-        grow_buffer += 5 
-        apple = spawn_food()
-        gold_apple = [-1, -1] # Hide the golden apple outside the map regardless, so there cannot be two golden apples in a row
+        if not grow_buffer > 0:
+            segments.pop() # Remove from the segments list (automatically the last item) if the snake is not supposed to grow
+        else:
+            grow_buffer -= 1
 
-    if not grow_buffer > 0:
-        segments.pop() # Remove from the segments list (automatically the last item) if the snake is not supposed to grow
-    else:
-        grow_buffer -= 1
+            
+        gold_apple_direction = [random.randint(-1, 1),  random.randint(-1, 1)] # The golden apple has legs
 
-        
-    gold_apple_direction = [random.randint(-1, 1),  random.randint(-1, 1)] # The golden apple has legs
+        # If the golden apple isn't hidden, it moves in a random direction
+        if gold_apple != [-1, -1]:
+            new_gold_apple_pos = [gold_apple[0] + gold_apple_direction[0], gold_apple[1] + gold_apple_direction[1]]
+            if new_gold_apple_pos not in segments and new_gold_apple_pos[0] >= 0 and new_gold_apple_pos[0] < x_map_size and new_gold_apple_pos[1] >= 0 and new_gold_apple_pos[1] < y_map_size:
+                gold_apple = new_gold_apple_pos
 
-    # If the golden apple isn't hidden, it moves in a random direction
-    if gold_apple != [-1, -1]:
-        new_gold_apple_pos = [gold_apple[0] + gold_apple_direction[0], gold_apple[1] + gold_apple_direction[1]]
-        if new_gold_apple_pos not in segments and new_gold_apple_pos[0] >= 0 and new_gold_apple_pos[0] < x_map_size and new_gold_apple_pos[1] >= 0 and new_gold_apple_pos[1] < y_map_size:
-            gold_apple = new_gold_apple_pos
-
-    Rendering()
-    time.sleep(ITERATION_DURATION)
-    if game_over == True:
-        break
+        Rendering()
+        if game_over == True:
+            break
 
